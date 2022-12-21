82e00fe1a4e26a6932120033fc0cfb703161c10d
coolpro200021 <72946059+coolpro200021@users.noreply.github.com>
2022-12-21 17:26:56 -0600

    playground

--- website/data/links.js
+++ website/data/links.js
@@ -6,7 +6,7 @@ const links = {
   twitter: 'https://twitter.com/AsynchronousAI',
   discord: 'https://discord.gg/vChUXVf9Em',
   license: 'https://github.com/AsynchronousAI/clay/blob/main/LICENSE.md',
-  playground: 'https://asynchronousai.github.io/clay/',
+  playground: 'https://asynchronousai.github.io/clay-playground/',
 };
 
 export default links;
