0fc3d60a0ff228ba0c99ed58719d1a79a0979649
coolpro200021 <72946059+coolpro200021@users.noreply.github.com>
2023-01-15 21:07:48 -0600

    tests

--- index.rs
+++ index.rs
@@ -1,7 +1,7 @@
 fn main(){
     // print a multiplication table with 10 rows and 10 columns and all of the numbers are aligned to the right
-    for i in 1..10001{
-        for j in 1..10001{
+    for i in 1..20{
+        for j in 1..20{
             print!("{:>4}", i*j);
         }
         println!();
